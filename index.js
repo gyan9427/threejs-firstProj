441c519104d3b1ef9fda268c0e6220a3e6da8cc9
gyan <gyan9427@gmail.com>
2021-07-25 22:23:13 +0530

    Install Vite create scene, add lightings, add toolbar

---
+++ index.js
@@ -0,0 +1,5 @@
+  // Find the latest version by visiting https://cdn.skypack.dev/three.
+      
+  import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.130.1-bsY6rEPcA1ZYyZeKdbHd/mode=imports,min/optimized/three.js';
+      
+  const scene = new THREE.Scene();
